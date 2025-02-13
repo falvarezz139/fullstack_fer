fc1e0c4bf47989fe1a2a701ffc54a21a8e801644
Fernando Alvarez <falvarez@lauraotero.com>
2025-02-13 14:49:32 +0100

    introduccion

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-import React, { useState, useEffect } from "react";
-
-function Visitas() {
-  const [visitas, setVisitas] = useState(0);
-
-  useEffect(() => {
-    const intervalo = setInterval(() => {
-      setVisitas((prevVisitas) => prevVisitas + 1);
-    }, 2000);
-
-    return () => {
-      clearInterval(intervalo);
-    };
-  }, []);
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-      <h1>Visitas: {visitas}</h1>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Ejercicio de Visitas</h1>
-      <Visitas />
-    </div>
-  );
-}
+};
 
 export default App;
