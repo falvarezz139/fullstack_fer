cfad7f91cb69b0be9c3171a81c52e5244875f89c
Fernando Alvarez <falvarez@lauraotero.com>
2025-02-14 09:41:49 +0100

    primer ejercicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,38 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const parts = [
+    { name: "Fundamentals of React", exercises: 10 },
+    { name: "Using props to pass data", exercises: 7 },
+    { name: "State of a component", exercises: 14 },
+  ];
 
+  const Header = ({ course }) => {
+    return <h1>{course}</h1>;
+  };
+  const Content = ({ parts }) => {
+    return (
+      <div>
+        {parts.map((parts, index) => (
+          <p key={index}>
+            {parts.name} {parts.exercises}
+          </p>
+        ))}
+      </div>
+    );
+  };
+  const Total = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+    return (
+      <p>
+        <strong>Total exercises: {total}</strong>
+      </p>
+    );
+  };
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
