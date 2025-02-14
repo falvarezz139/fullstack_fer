36275a31197ade03ce90415f45e413a0f9ed651d
Fernando Alvarez <falvarez@lauraotero.com>
2025-02-14 09:53:45 +0100

    segundo ejer

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
 const App = () => {
-  const course = "Half Stack application development";
   const parts = [
     { name: "Fundamentals of React", exercises: 10 },
     { name: "Using props to pass data", exercises: 7 },
     { name: "State of a component", exercises: 14 },
   ];
 
-  const Header = ({ course }) => {
-    return <h1>{course}</h1>;
-  };
-  const Content = ({ parts }) => {
-    return (
-      <div>
-        {parts.map((parts, index) => (
-          <p key={index}>
-            {parts.name} {parts.exercises}
-          </p>
-        ))}
-      </div>
-    );
-  };
-  const Total = ({ parts }) => {
-    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  const Part = ({ name, exercises }) => {
     return (
       <p>
-        <strong>Total exercises: {total}</strong>
+        {name} {exercises}
       </p>
     );
   };
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Part name={parts[1].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </div>
   );
 };
